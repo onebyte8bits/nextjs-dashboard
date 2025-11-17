92fa0293e9eaf7e8ec1387a85b2843ef2e45c758
wangran5 <Ran5.Wang@cicc.com.cn>
2025-11-17 11:59:20 +0800

    nextjs demo

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  console.log('loading')
+  return (
+    <>
+      <h1>页面加载中...</h1>
+    </>
+  )
+}
---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: ['http://10.119.219.72'],
+};
+
+export default nextConfig;
